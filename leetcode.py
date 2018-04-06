e39f81a22ed1024547d62ad096840d606c430d07
CHANDIM <sijie_chen@outlook.com>
2018-04-06 15:27:12 +0200

    formatize and add main

--- leetcode.py
+++ leetcode.py
@@ -7,31 +7,34 @@ class Solution(object):
         index = 0
         
         for num in nums:
-            
-            # check if current number is less than targezt, if yes 
-            if num <= target:
                 
-                # check if it's been expected, if no
-                if not checkPresence(num, potentialNumbers):
-                    
-                    # and store index
-                    potentialNumbers[target - num] = index
-                    
-                else :
-                    index1 = potentialNumbers.get(num)
-                    index2 = index
-                    return [index1, index2]
+            # check if it's been expected, if no
+            if not checkPresence(num, potentialNumbers):
+                
+                # and store index
+                potentialNumbers[target - num] = index
+                
+            else :
+                index1 = potentialNumbers.get(num)
+                index2 = index
+                return [index1, index2]
             index = index + 1
                     
+
 def checkPresence(num, group):
     for key, value in group.items():
         if num == key:
             return True
     return False
         
-                      
-solution = Solution()
-nums = [0,4,3,0]
-target = 0
 
-print(solution.twoSum(nums, target))
+def main():
+
+    solution = Solution()
+    nums = [-3,4,3,90]
+    target = 87
+    print(solution.twoSum(nums, target))
+
+
+if __name__ == '__main__':
+    main()
