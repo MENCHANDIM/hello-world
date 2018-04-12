d4932226d18b6d040f910e9648f8ea95ea803ba2
CHANDIM <sijie_chen@outlook.com>
2018-04-12 11:17:53 +0200

    add each other and find the exact sum

--- leetcode.py
+++ leetcode.py
@@ -1,38 +1,55 @@
+# class Solution(object):
+#     def twoSum(self, nums, target):
+
+#         # waiting list    
+#         potentialNumbers = {}
+        
+#         index = 0
+        
+#         for num in nums:
+                
+#             # check if it's been expected, if no
+#             if not checkPresence(num, potentialNumbers):
+                
+#                 # and store index
+#                 potentialNumbers[target - num] = index
+                
+#             else :
+#                 index1 = potentialNumbers.get(num)
+#                 index2 = index
+#                 return [index1, index2]
+#             index = index + 1
+                    
+
+# def checkPresence(num, group):
+#     for key, value in group.items():
+#         if num == key:
+#             return True
+#     return False
+        
+
+# def main():
+
+#     solution = Solution()
+#     nums = [-3,4,3,90]
+#     target = 87
+#     print(solution.twoSum(nums, target))
+
+
 class Solution(object):
     def twoSum(self, nums, target):
 
-        # waiting list    
-        potentialNumbers = {}
+        length = len(nums)
         
-        index = 0
-        
-        for num in nums:
-                
-            # check if it's been expected, if no
-            if not checkPresence(num, potentialNumbers):
-                
-                # and store index
-                potentialNumbers[target - num] = index
-                
-            else :
-                index1 = potentialNumbers.get(num)
-                index2 = index
-                return [index1, index2]
-            index = index + 1
-                    
-
-def checkPresence(num, group):
-    for key, value in group.items():
-        if num == key:
-            return True
-    return False
-        
+        for i in range(0, length):
+            for j in range(i+1, length):
+                if target == nums[i] + nums[j]:
+                    return [i,j]
 
 def main():
-
     solution = Solution()
-    nums = [-3,4,3,90]
-    target = 87
+    nums = [899,4,3,90]
+    target = 903
     print(solution.twoSum(nums, target))
 
 
